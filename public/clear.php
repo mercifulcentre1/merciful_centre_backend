62f04a15bd9a0256cad2001a291925a798bd4841
TemitopeGX <temitopeayomikun999@gmail.com>
2026-08-18 18:03:05 +0100

    fix: aggressively delete route cache files

--- public/clear.php
+++ public/clear.php
@@ -4,5 +4,13 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
+// Manually delete cache files just to be 100% sure
+@unlink(__DIR__.'/../bootstrap/cache/routes-v7.php');
+@unlink(__DIR__.'/../bootstrap/cache/routes-v7.php.php'); // Sometimes it has an extra .php
+@unlink(__DIR__.'/../bootstrap/cache/config.php');
+@unlink(__DIR__.'/../bootstrap/cache/events.php');
+@unlink(__DIR__.'/../bootstrap/cache/services.php');
+@unlink(__DIR__.'/../bootstrap/cache/packages.php');
+
 \Illuminate\Support\Facades\Artisan::call('optimize:clear');
-echo "Cache completely cleared successfully!";
+echo "Cache completely cleared successfully (and files deleted manually)!";
