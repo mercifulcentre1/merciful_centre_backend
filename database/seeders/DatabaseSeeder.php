e6944c55b088c2b8ccae81422e345f662195e107
TemitopeGX <temitopeayomikun999@gmail.com>
2026-08-18 16:21:38 +0100

    feat: add initial AdminUser seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\AdminUser;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,5 +21,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        AdminUser::create([
+            'username' => 'MercifulCentre',
+            'email' => 'admin@mercifulcentre.org',
+            'full_name' => 'Super Admin',
+            'password_hash' => Hash::make('Merciful12345'),
+            'role' => 'super_admin',
+        ]);
     }
 }
